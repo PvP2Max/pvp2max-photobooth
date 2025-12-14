b11c23594e244f041f2d8ff907b224c88386a5b1
kamronalexander <kalexander258@gmail.com>
2025-12-13 19:43:03 -0900

    Remove lightningcss disable hack; will keep build flags in Docker

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
-// Force-disable lightningcss so builds don't require native binaries
-if (!process.env.NEXT_DISABLE_LIGHTNINGCSS) process.env.NEXT_DISABLE_LIGHTNINGCSS = "1";
-if (!process.env.TAILWIND_DISABLE_LIGHTNINGCSS) process.env.TAILWIND_DISABLE_LIGHTNINGCSS = "1";
-
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
-  experimental: {
-    optimizeCss: false, // disable lightningcss native binary in slim images
-  },
 };
 
 export default nextConfig;
